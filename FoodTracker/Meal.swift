ce29d4173230f65e07e2d41056e47180f98f6510
Yehan Pemarathne <yehancha@gmail.com>
2019-03-15 13:07:15 +0530

    Add Meal class

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,32 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Yehan Pemarathne on 3/15/19.
+//  Copyright © 2019 Yehan Pemarathne. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+ 
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
